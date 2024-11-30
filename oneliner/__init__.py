a5316ceb90e07150e8d1eba65db612fe63a2fdad
yunline <yunline@foxmail.com>
2024-11-30 09:40:34 +0800

    Use customized unparser instead of ast.unparse

--- oneliner/__init__.py
+++ oneliner/__init__.py
@@ -1,4 +1,5 @@
 from oneliner.convert import OnelinerConvertor
+from oneliner.expr_unparse import unparse
 
 __all__ = ["OnelinerConvertor", "convert_code_string"]
 
@@ -10,4 +11,4 @@ def convert_code_string(code: str, filename="<string>"):
     ast_root = ast.parse(code, filename, "exec")
     symtable_root = symtable.symtable(code, filename, "exec")
     out = OnelinerConvertor().cvt(ast_root, symtable_root)
-    return ast.unparse(out).replace("\n", "")
+    return unparse(out)
