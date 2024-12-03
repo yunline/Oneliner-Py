ed0d5fb4fc9c2231d37ba3a6fba07ae3b7614ea8
yunline <yunline@foxmail.com>
2024-12-03 09:23:43 +0800

    Add `-v` and `--unparser` argument Allow users to check the version and choose the unparser. By default the unparser will be ast.unparse for compatibility

--- oneliner/__main__.py
+++ oneliner/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 
 import oneliner
+import oneliner.version
 
 parser = argparse.ArgumentParser(
     description="Convert python scripts into oneliner expression."
@@ -12,6 +13,15 @@ parser.add_argument(
     help="The filename of the python script to be converted",
 )
 
+ver = oneliner.version.oneliner_version
+dev = "-dev" if oneliner.version.dev else ""
+parser.add_argument(
+    "-v",
+    "--version",
+    action="version",
+    version=f"Oneliner-Py-{ver[0]}.{ver[1]}.{ver[2]}{dev}",
+)
+
 parser.add_argument(
     "-o",
     "--output",
@@ -20,12 +30,22 @@ parser.add_argument(
     "Oneliner-Py will print the result to the screen.",
 )
 
+parser.add_argument(
+    "--unparser",
+    type=str,
+    default="ast.unparse",
+    choices=["ast.unparse", "oneliner"],
+)
+
 args = parser.parse_args()
 
 with open(args.input_filename, "r", encoding="utf8") as infile:
     script = infile.read()
 
-converted = oneliner.convert_code_string(script)
+if args.unparser == "ast.unparse":
+    converted = oneliner.convert_code_string(script, use_new_unparser=False)
+else:
+    converted = oneliner.convert_code_string(script, use_new_unparser=True)
 
 if args.output is not None:
     with open(args.output, "w", encoding="utf8") as outfile:
