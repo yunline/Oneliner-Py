0ed3575007e9cc03a0d96ddc228b49af50d874ae
yunline <yunline@foxmail.com>
2024-12-12 20:21:40 +0800

    Add tests for expr_unparse

--- oneliner_tests/__main__.py
+++ oneliner_tests/__main__.py
@@ -1,5 +1,6 @@
 import unittest
 
-from . import oneliner_test
+from . import oneliner_test, unparser_test
 
-unittest.main(oneliner_test)
+unittest.main(unparser_test.__name__, exit=False)
+unittest.main(oneliner_test.__name__)
